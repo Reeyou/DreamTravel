0549cc0846e7e92b910f7d61dc2581c367c34c29
Reeyou <ryu993830137@163.com>
2019-04-11 21:51:23 +0800

    merge push

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div id="app" v-cloak>
+  <div id="app">
+    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -15,9 +16,8 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  box-sizing: border-box;
-  /* text-align: center; */
+  text-align: center;
   color: #2c3e50;
-  /* margin-top: 60px; */
+  margin-top: 60px;
 }
 </style>
